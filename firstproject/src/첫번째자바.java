0fb35dbb098b0b762e87f081ddfae6a5e868ba25
SIST-111 <SIST-111@SIST>
2024-11-18 14:20:45 +0900

    2024-11-18

---
+++ firstproject/src/첫번째자바.java
@@ -0,0 +1,74 @@
+/*
+     1. 대문자 구분
+     2. 들여쓰기 => 가독성
+     3. 명령문 종료 반드시 ;
+        ; => 파이썬 / kotlin / javascript
+     4. 자바는 {
+     5. 문법
+        데이터저장  ====> 데이터 처리(요청)  =====> 화면 출력
+           ㅣ               ㅣ                   ㅣ
+                         연산자/제어문         명령프롬프트 :  application
+                                               문법
+                       =============       브라우저 출력 : web application
+                                               주력
+                           메소드             모바일 출려 : mobile application   
+        한개저장(메모리) : 변수/상수               spring : java / kotlin
+        여러 개 저장 : 배열
+                    클래스
+        -----------------------------------------------
+          프로그램 종료시 사라진다
+         파일 / 오라클  => 영구적인 저장      
+
+     ==================================> 기본문법
+     1. 기능을 모아서 관리
+        ========
+           ===> 단락 :  메소드
+           ===> 클래스 / 객체 => 객제지향 프로그램
+           ===> 프로그램 유지 (예외처리)
+           ================================ 8장
+           ===> 라이브러리 : 자바에 지원
+           ===> 1차 응용포로그램
+     ===========================> web
+     1. html : 브라우저에서 실행
+               자바는 브라우저에서는 일반텍스트
+        ==> 투박 => css / 자바스크립트
+     2. jsp (요청 => html => java)
+             응답 => java => html)
+            mvc
+            ===
+            front / back
+              ㅣ      ㅣ 자바/오라클/spring
+           js / ts                
+        ================================= full stack
+     3. 통합 : framework : spring
+              기본 틀 => 메인보드
+     4. 데이터 분석 : python / numpy / pandas / matplotlip
+     5. nosql => elasticsearch 
+                  ㅣ 오라클 역할 ㅣ
+     6. 배포 :  aws
+     ==========================================
+     spring-boot / jpa / my sql / react-query
+     redux / vue3 / nextjs / typescript / nodejs
+     
+     자바 / 오라클 / 알고리즘
+     html/css/javascript/jsp/
+     spring-boot/pythin/데이터분석
+     elasticserch
+     ==========================================
+     spring+mybatis+jsp+javascript(jquery)                          
+        
+        
+ */
+public class 첫번째자바 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       System.out.println("hello java");
+       System.out.println("정주희");
+       {
+    	   
+       }
+      
+	}
+
+}
