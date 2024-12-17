a35ee9834ef079550bfb69520fa72151f925b0bf
SIST-111 <SIST-111@SIST>
2024-12-17 13:58:48 +0900

    20241213

---
+++ Chap_06상속(응용)_1/src/com/sist/main/MusicDetailPanel.java
@@ -0,0 +1,10 @@
+package com.sist.main;
+import java.awt.Color;
+import javax.swing.JPanel;
+public class MusicDetailPanel extends JPanel{
+    public MusicDetailPanel()
+    {
+    	setBackground(Color.cyan);
+    }
+
+}
