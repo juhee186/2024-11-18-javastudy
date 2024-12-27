ca09a81f6938ca7f45c018d1c08801f0bbc06160
SIST-111 <SIST-111@SIST>
2024-12-27 17:33:17 +0900

    20241227

---
+++ Chap_11IO라이브러리_5/src/com/sist/main/EmpDAO.java
@@ -0,0 +1,78 @@
+package com.sist.main;
+import java.util.*;
+import java.io.*;
+import java.sql.*;
+public class EmpDAO {
+	private Connection conn;
+	private PreparedStatement ps;
+	private String driver,url,username,password;
+	
+	public EmpDAO()
+	{
+		//properties 읽기
+		try
+		{
+			Properties prop=new Properties();
+			prop.load(new FileInputStream("C:\\javadev\\javastudy\\Chap_11IO라이브러리_5\\src\\com\\sist\\main\\db.properties"));
+			driver=prop.getProperty("driver");
+			url=prop.getProperty("url");
+			username=prop.getProperty("username");
+			password=prop.getProperty("password");
+			//보완용 =>  데이터베이스 / 네트워크서버
+			// 드라이버 설정
+			Class.forName(driver);
+			
+		}catch(Exception ex)
+	
+		{
+			ex.printStackTrace();
+		}
+	}
+	// 연결
+	// => 위탁
+		public void getConnection()
+		{
+			try
+			{
+				conn=DriverManager.getConnection(url,username,password);
+			}catch(Exception ex) {}
+		}
+		public void disConnection()
+		{
+			try
+			{
+				if(ps!=null) ps.close();
+				if(conn!=null)conn.close();
+			}catch(Exception ex) {}
+		}
+		public List<Emp> empListData()
+		{
+			List<Emp> list=new ArrayList<Emp>();
+			try
+			{
+				getConnection();
+				String sql="SELECT empno,ename,job FROM emp";
+						ps=conn.prepareStatement(sql);
+				ResultSet rs=ps.executeQuery();
+				while(rs.next())
+				{
+					Emp emp=new Emp();
+					emp.setEmpno(rs.getInt(1));
+					emp.setEname(rs.getString(2));
+					emp.setJob(rs.getString(3));
+					list.add(emp);
+				}
+			}catch(Exception ex)
+			{
+				ex.printStackTrace();
+			}
+			finally
+			{
+				disConnection();
+			}
+			return list;
+			
+		}
+	}
+	
+
---
+++ Chap_11IO라이브러리_5/src/com/sist/main/Properties_1.java
@@ -0,0 +1,23 @@
+package com.sist.main;
+
+import java.util.*;
+public class Properties_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		Properties props=System.getProperties();
+//		System.out.println(props.getProperty("java.version"));
+//		System.out.println(props.getProperty("user.language"));
+		
+		EmpDAO dao=new EmpDAO();
+		List<Emp> list=dao.empListData();
+		for(Emp e:list)
+		{
+			System.out.println(e.getEmpno()+" "
+					+e.getEname()+" "
+					+e.getJob());
+		}
+
+	}
+
+}
